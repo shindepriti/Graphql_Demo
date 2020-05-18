8d38f2152d548de2c87e59e6ba68b3bf80d48ea3
shindepriti <pritishinde34@gmail.com>
2020-05-19 00:51:10 +0530

    ADDED:Define Data Set For Book Type

---
+++ index.js
@@ -0,0 +1,13 @@
+const {AppolloServer,gql} = require("apollo-server")
+const typeDef = gql
+
+const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ];
